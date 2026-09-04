bcbc1e282751f042621867ac2b803bfd6a6d20d3
Lucas Simpson <lucassimpson0213@gmail.com>
2026-09-03 20:18:51 -0400

    added animations back

--- hyprland.lua
+++ hyprland.lua
@@ -147,7 +147,7 @@ hl.config({
     },
 
     animations = {
-        enabled = false,
+        enabled = true,
     },
 
     dwindle = {
@@ -172,41 +172,41 @@ hl.config({
 ---- ANIMATIONS ---
 ------------------
 
--- hl.curve("easeOutQuint", {
---     type = "bezier",
---     points = { { 0.23, 1 }, { 0.32, 1 } },
--- })
---
--- hl.curve("linear", {
---     type = "bezier",
---     points = { { 0, 0 }, { 1, 1 } },
--- })
---
--- hl.curve("almostLinear", {
---     type = "bezier",
---     points = { { 0.5, 0.5 }, { 0.75, 1 } },
--- })
---
--- hl.curve("quick", {
---     type = "bezier",
---     points = { { 0.15, 0 }, { 0.1, 1 } },
--- })
---
--- hl.curve("easy", {
---     type = "spring",
---     mass = 1,
---     stiffness = 71.2633,
---     dampening = 15.8274,
--- })
---
--- -- hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
--- -- hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
--- -- hl.animation({ leaf = "windows", enabled = true, speed = 4.79, spring = "easy" })
--- -- hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, spring = "easy", style = "popin 87%" })
--- -- hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.49, bezier = "linear", style = "popin 87%" })
--- -- hl.animation({ leaf = "fade", enabled = true, speed = 3.03, bezier = "quick" })
--- -- hl.animation({ leaf = "layers", enabled = true, speed = 3.81, bezier = "easeOutQuint" })
--- hl.animation({ leaf = "workspaces", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.curve("easeOutQuint", {
+    type = "bezier",
+    points = { { 0.23, 1 }, { 0.32, 1 } },
+})
+
+hl.curve("linear", {
+    type = "bezier",
+    points = { { 0, 0 }, { 1, 1 } },
+})
+
+hl.curve("almostLinear", {
+    type = "bezier",
+    points = { { 0.5, 0.5 }, { 0.75, 1 } },
+})
+
+hl.curve("quick", {
+    type = "bezier",
+    points = { { 0.15, 0 }, { 0.1, 1 } },
+})
+
+hl.curve("easy", {
+    type = "spring",
+    mass = 1,
+    stiffness = 71.2633,
+    dampening = 15.8274,
+})
+
+hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
+hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
+hl.animation({ leaf = "windows", enabled = true, speed = 4.79, spring = "easy" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, spring = "easy", style = "popin 87%" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.49, bezier = "linear", style = "popin 87%" })
+hl.animation({ leaf = "fade", enabled = true, speed = 3.03, bezier = "quick" })
+hl.animation({ leaf = "layers", enabled = true, speed = 3.81, bezier = "easeOutQuint" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
 
 ---------------
 ---- INPUT ----
